2e9258745d3c41f79828161962c0c36d7c9af90c
eetp495rafaela <soportetecnico.tec495@gmail.com>
2026-08-06 15:25:44 -0300

    Arreglo commit anterior que no se hizo

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -348,7 +348,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   /* =====================================================
-     EVENTOS
+     EVENTOS GALERIA
   ===================================================== */
 
   categoriasGaleria.forEach((categoria) => {
